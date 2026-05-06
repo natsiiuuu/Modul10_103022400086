81823cb944732841b926dfb35a509e0f53834034
natsiiuuu <fattanahmad898@gmail.com>
2026-05-06 07:01:30 +0700

    first commit

---
+++ Modu10_103022400086/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Modu10_103022400086
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
